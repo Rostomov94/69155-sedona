932a4dcd06f4bab58d774405a5029f99c74f61b7
Анатолий <antolli994@gmail.com>
2018-05-10 09:04:37 +0400

    подключил пакеты через npm

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,58 @@
 
 var gulp = require("gulp");
 var less = require("gulp-less");
+var minify = require("gulp-csso");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var webp = require ("gulp-webp");
 var server = require("browser-sync").create();
+
+gulp.task("webp", function() {
+    return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("images", function() {
+    return gulp.src("source/img/**/*.{png, jpg, svg}") 
+        .pipe(imagemin([
+            imagemin.optipng({optimizationlevel: 3}), 
+            imagemin.jpegtran({progressive: true}),
+            imagemin.svgo()                           
+        ]))
+        .pipe(gulp.dest("source/img"));
+});
 
 gulp.task("style", function() {
   gulp.src("source/less/style.less")
-    .pipe(plumber())
+    .pipe(gulp-plumber())
     .pipe(less())
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(gulp-rename())
+    .pipe(gulp.dest("source/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
 });
 
+
+
 gulp.task("serve", ["style"], function() {
   server.init({
     server: "source/",
-    notify: false,
+    notify: false,  
     open: true,
     cors: true,
     ui: false
   });
 
   gulp.watch("source/less/**/*.less", ["style"]);
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html").on("change", server.reload); 
+ 
 });
