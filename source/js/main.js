9cfa758bd31dc337900deeb90abfaebc98016635
Анатолий <antolli994@gmail.com>
2018-05-14 13:39:37 +0400

    Начинаю переделывать ФОРМУ

--- source/js/main.js
+++ source/js/main.js
@@ -1,43 +1,13 @@
 var Nav = document.querySelector('.nav__list');
-var Nav__toggle = document.querySelector('.nav__toggle-open');
-
+var Nav__toggle = document.querySelector('.nav__toggle');
 
 Nav__toggle.addEventListener('click', function () {
-    
 	if (Nav.classList.contains('nav__list--closed')) {
-        
 		Nav.classList.remove('nav__list--closed');
 		Nav.classList.add('nav__list--open');
         
-	}
-    
-	else {
-        
+	} else {
 		Nav.classList.remove('nav__list--open');
 		Nav.classList.add('nav__list--closed');
 	}
-
-});
-
-
-var Nav = document.querySelector('.nav__list');
-var Nav__toggle = document.querySelector('.nav__toggle-close');
-
-
-Nav__toggle.addEventListener('click', function () {
-    
-	if (Nav.classList.contains('nav__list--open')) {
-        
-		Nav.classList.remove('nav__list--open');
-		Nav.classList.add('nav__list--closed');
-        
-	}
-    
-	else {
-        
-		Nav.classList.remove('nav__list--closed');
-		Nav.classList.add('nav__list--open');
-        
-	}
-
 });
